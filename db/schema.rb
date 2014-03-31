70bf79e3e2f498dfa7e30504ad1994415e048893
jszwedko <jesse.szwedko@gmail.com>
2014-03-30 22:19:50 -0400

    Adding due date to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140331015540) do
+ActiveRecord::Schema.define(version: 20140331021744) do
 
   create_table "tasks", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "due"
   end
 
 end
